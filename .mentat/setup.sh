9d6ab06265422e0ed3eaa4dc3cbc71d7a38c56c8
Jake Koenig <jakethekoenig@gmail.com>
2025-05-21 10:59:49 -0700

    Install mentat deps

--- .mentat/setup.sh
+++ .mentat/setup.sh
@@ -3,3 +3,7 @@
 # Install dependencies for both client and server
 # The root package.json has an install script that handles this
 npm install
+apt-get update
+apt-get install -y lsof
+apt-get install -y iptables
+apt-get install -y socat
